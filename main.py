4c154b0ec4675fa1170607561a80470185140b1d
Lawrence Lewis <lel486@gmail.com>
2021-09-07 17:17:01 -0400

    add code...

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-""" program to read and use FRED interest rates,
-and a function to return interpolated rate for any period from 1 day to 360 days
+""" program to read and use FRED interest rates, and a function to return interpolated rate for any period
+from 0 days to 360 days.
 
 The intention is that this will be used for things like the risk free rate for Black Scholes computations.
 
@@ -12,25 +12,71 @@ will be a key for every day of the year from 1/1/1986 forward
 
 After 12/31/2021, LIBOR rates will be replaced by SOFR and this code will be modified appropriately so the caller
 doesn't need to know whether the rate comes from the LIBOR dtatbase or SOFR database
-
 """
 
-import pandas as pd
+import math
+import numpy
+import pandas
+import datetime
+from datetime import timedelta
 
-FRED_interest_rates = {1:dict(), 7:dict(), 30:dict(), 60:dict(), 90:dict(), 180:dict(), 360:dict()}
+FRED_interest_rates = {1: {'name': 'USDONTD156N'}, 7: {'name': 'USD1WKD156N'}, 30: {'name': 'USD1MTD156N'},
+                       60: {'name': 'USD2MTD156N'}, 90: {'name': 'USD3MTD156N'}, 180: {'name': 'USD6MTD156N'},
+                       360: {'name': 'USD12MD156N'}}
+global_first_date = numpy.datetime64('1986-01-01')
 
 def read_FRED_interest_rates():
     global FRED_interest_rates
+    global global_first_date
 
-    fred_series_names = {'USDONTD156N': 1, 'USD1WKD156N': 7, 'USD1MTD156N': 30, 'USD2MTD156N': 60, 'USD3MTD156N': 90,
-                         'USD6MTD156N': 180, 'USD12MD156N': 360}
-    fred_url = 'https://fred.stlouisfed.org/graph/fredgraph.csv?id={series}&cosd=2016-08-27&coed=2021-08-27'
+    global_first_date = pandas.Timestamp('1986-01-01')
+    today = pandas.Timestamp.today()
+    today_str = today.strftime('%Y-%m-%d')
+    for duration, series in FRED_interest_rates.items():
+        series_name = series['name']
+        FRED_url = f'https://fred.stlouisfed.org/graph/fredgraph.csv?id={series_name}&cosd=1985-01-01&coed={today_str}'
+        rate_df = pandas.read_csv(FRED_url, header=0, names=['date', 'rate'], parse_dates=[0], na_values={'rate': '.'},
+                                  keep_default_na=False, engine='c')
+        rate_df.sort_values(by='date', inplace=True)
+        series['df'] = rate_df
 
-    series = fred_series_names[]
+        # keep track of overall earliest date of all series read
+        first_date = rate_df.iloc[0].date
+        if first_date > global_first_date:
+            global_first_date = first_date
+
+        # for informational purposes only
+        print(series_name)
+        row = rate_df.iloc[0]
+        print(row.date.date(), row.rate)
+        lastrow = len(rate_df.index) - 1
+        row = rate_df.iloc[len(rate_df.index) - 1]
+        print(row.date.date(), row.rate)
+        print()
+        break
+
+    # now create numpy array with 1 row for every day between global_first_date and today
+    # so, in order to grab a rate we just compute # of days between requested date and global_first_dat, and that's the
+    # index into the numpy array
+    numrows = ((today - global_first_date) + timedelta(1)).days
+    for duration, series in FRED_interest_rates.items():
+        rate_array = numpy.full(numrows, numpy.nan, numpy.float)
+        rate_df = series['df']
+        prev_date = global_first_date - timedelta(1)
+        for index, row in rate_df.iterrows():
+            i = (row[0] - global_first_date).days
+            rate_array[i] = row[1]
+        # now use pandas interpolate method to remove NaN's
+        pandas_series = pandas.Series(rate_array)
+        pandas_series.interpolate(inplace=True)
+        rate_array = pandas_series.values
+        y = 1
+
+    print("Starting date will be: ", global_first_date)
 
 
-def FRED_interest_rate(duration: int) -> float:
-    return 0;
+def FRED_interest_rate(date: datetime, duration: int) -> float:
+    return 0
 
 
 if __name__ == '__main__':
